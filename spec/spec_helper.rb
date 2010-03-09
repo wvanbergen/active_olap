e30687d115dcff9c45953f47678a197d577a7393
Willem van Bergen <willem@vanbergen.org>
2010-03-09 13:11:43 -0500

    Removed old tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,8 @@
-$:.reject! { |e| e.include? 'TextMate' }
 $:.unshift(File.dirname(__FILE__) + '/../lib')
 
 # load RSpec libraries
 require 'rubygems'
-gem 'rspec', '>=1.1.11'
-require 'test/unit'
 require 'spec'
 
-
 # Load Active OLAP files
-require 'active_record'
 require 'active_olap'
